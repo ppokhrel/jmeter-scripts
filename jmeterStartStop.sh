7cd55c1ad2072619207f9ff821ea4ca13de39585
ppokhrel <techpj@gmail.com>
2019-08-01 14:43:26 -0700

    Update jmeterStartStop.sh

--- jmeterStartStop.sh
+++ jmeterStartStop.sh
@@ -2,5 +2,5 @@
 for i in 010
 do
 echo "Starting SSH to slcd000ldr${i}"
-ssh -t ppokhrel@slcd000ldr${i}.stubcorp.com "echo \"Stopping Jmeter Service\"; sudo systemctl stop jmeter-server.service; echo \"Starting Jmeter Service\";sudo systemctl start jmeter-server.service; echo \"Jmeter Services Running:\"; pgrep jmeter"
+ssh -t <username>@<host>${i}.<domain>.com "echo \"Stopping Jmeter Service\"; sudo systemctl stop jmeter-server.service; echo \"Starting Jmeter Service\";sudo systemctl start jmeter-server.service; echo \"Jmeter Services Running:\"; pgrep jmeter"
 done
