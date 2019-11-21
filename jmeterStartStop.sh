d660eb54a1908120a94794f6f318c995fe05c878
Prapunja (PJ) Pokhrel <techpj@gmail.com>
2019-11-21 10:59:32 -0800

    Update jmeterStartStop.sh

--- jmeterStartStop.sh
+++ jmeterStartStop.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+# assume there are 10 servers with host name = *<id>.somedomain.com
+# replace host and username 
 for i in 010
 do
-echo "Starting SSH to <host>${i}"
-ssh -t <username>@<host>${i}.<domain>.com "echo \"Stopping Jmeter Service\"; sudo systemctl stop jmeter-server.service; echo \"Starting Jmeter Service\";sudo systemctl start jmeter-server.service; echo \"Jmeter Services Running:\"; pgrep jmeter"
+echo "Starting SSH to <somehost>${i}"
+ssh -t <yourusername>@<host>${i}.<yourdomain>.com "echo \"Stopping Jmeter Service\"; sudo systemctl stop jmeter-server.service; echo \"Starting Jmeter Service\";sudo systemctl start jmeter-server.service; echo \"Jmeter Services Running:\"; pgrep jmeter"
 done
